999aeb94fa49045c04c544a6be55e864895bbb92
Lucas <lucasdasilvabarbosa27@gmail.com>
2021-02-09 17:42:48 -0300

    Material UI configuration
    Configuração do material UI do projeto.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
-
+import {Button} from '@material-ui/core'
 export default function Home() {
   return (
     <div >
       Clone Youtube Ui with next js
+      <Button variant="contained" color="primary">
+        Botão inicial
+      </Button>
     </div>
   )
 }
